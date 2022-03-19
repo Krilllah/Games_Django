b175dc79300db415846d20ad7580c39dcffa6fea
kandreev2023 <lileandr63@gmail.com>
2022-03-19 11:03:15 +0300

    test

--- industries/views.py
+++ industries/views.py
@@ -1,33 +1,73 @@
 from django.http import HttpResponse, HttpResponseRedirect
 from django.template.loader import render_to_string
+from django.shortcuts import render
 
 dct = {
-    'list': '<h1>Hello, there are list of game industries:</h1><br>Electronic_Arts/<br>Nintendo/<br>Activision_Blizzard/',
-    'Electronic_Arts': '<h1>Here are our franchises:</h1><br>Battlefield/<br>Plants_vs_Zombies/',
-    'Nintendo': '<h1>Here are our franchises:</h1><br>The_Legend_of_Zelda/<br>Donkey_Kong/',
-    'Activision_Blizzard': '<h1>Here are our franchises:</h1><br>Call_of_Duty/<br>Warcraft/',
-    'Activision_Blizzard/Warcraft ': 'List:<br>Warcraft: Orcs & Humans,<br>Warcraft II: Tides of Darkness,<br>'
-                        ' 	Warcraft II: Beyond the Dark Portal,<br> 	Warcraft II: Battle.net Edition,<br>'
-                        'Warcraft III: Reign of Chaos,<br>Warcraft III: The Frozen Throne,<br> World of Warcraft,<br>'
-                        '	World of Warcraft: The Burning Crusade,<br>	World of Warcraft: Wrath of the Lich King,<br> '
-                        'World of Warcraft: Cataclysm,<br>World of Warcraft: Mists of Pandaria,<br> Hearthstone,<br> '
-                        'World of Warcraft: Warlords of Draenor,<br> 	World of Warcraft: Legion,<br> 	'
-                        'World of Warcraft: Battle for Azeroth,<br>World of Warcraft Classic,<br>Warcraft III: Reforged,<br> '
-                        'World of Warcraft: Shadowlands, <br>	World of Warcraft: The Burning Crusade Classic.',
-    'Activision_Blizzard/Call_of_Duty': 'List:<br>Call of Duty<br>Call of Duty: United Offensive<br>Call of Duty: Finest Hour<br>'
-                                        'Call of Duty 2<br>Call of Duty 2: Big Red One<br>Call of Duty 3<br>Call of Duty: Roads to Victory<br>'
-                                        'Call of Duty 4: Modern Warfare<br>'
+    'list': {'intro': 'Hello, there are list of game industries:',
+             'description': ['Electronic_Arts', 'Nintendo', 'Activision_Blizzard']},
+    'Electronic_Arts': {'intro': 'Here are our franchises:', 'description': ['Battlefield', 'Plants_vs_Zombies']},
+    'Nintendo': {'intro': 'Here are our franchises:', 'description': ['The_Legend_of_Zelda', 'Donkey_Kong']},
+    'Activision_Blizzard': {'intro': 'Here are our franchises:', 'description': ['Call_of_Duty', 'Warcraft']},
+    'Warcraft ': {'intro': 'List:', 'description': ['Warcraft: Orcs & Humans', 'Warcraft II: Tides of Darkness',
+                                                    'Warcraft II: Beyond the Dark Portal',
+                                                    'Warcraft II: Battle.net Edition',
+                                                    'Warcraft III: Reign of Chaos', 'Warcraft III: The Frozen Throne',
+                                                    'World of Warcraft',
+                                                    'World of Warcraft: The Burning Crusade',
+                                                    'World of Warcraft: Wrath of the Lich King',
+                                                    'World of Warcraft: Cataclysm',
+                                                    'World of Warcraft: Mists of Pandaria', 'Hearthstone',
+                                                    'World of Warcraft: Warlords of Draenor',
+                                                    'World of Warcraft: Legion',
+                                                    'World of Warcraft: Battle for Azeroth',
+                                                    'World of Warcraft Classic', 'Warcraft III: Reforged',
+                                                    'World of Warcraft: Shadowlands',
+                                                    'World of Warcraft: The Burning Crusade Classic']},
+    'Call_of_Duty': {'intro': 'List:',
+                     'description': ['Call of Duty', 'Call of Duty: United Offensive', 'Call of Duty: Finest Hour',
+                                     'Call of Duty 2', 'Call of Duty 2: Big Red One', 'Call of Duty 3',
+                                     'Call of Duty: Roads to Victory',
+                                     'Call of Duty 4: Modern Warfare',
+                                     'Call of Duty: World at War',
+                                     'Call of Duty: World at War – Final Fronts',
+                                     'Call of Duty: World at War – Zombies',
+                                     'Call of Duty: Modern Warfare 2',
+                                     'Call of Duty Classic',
+                                     'Call of Duty: Black Ops',
+                                     'Call of Duty: Black Ops Zombies',
+                                     'Call of Duty: Modern Warfare 3',
+                                     'Call of Duty: Black Ops II',
+                                     'Call of Duty: Black Ops Declassified',
+                                     'Call of Duty Online',
+                                     'Call of Duty: Strike Team',
+                                     'Call of Duty: Ghosts',
+                                     'Call of Duty: Heroes',
+                                     'Call of Duty: Advanced Warfare',
+                                     'Call of Duty: Black Ops III',
+                                     'Call of Duty: Infinite Warfare',
+                                     'Call of Duty: Modern Warfare Remastered',
+                                     'Call of Duty: WWII',
+                                     'Call of Duty: Black Ops 4',
+                                     'Call of Duty: Mobile',
+                                     'Call of Duty: Modern Warfare',
+                                     'Call of Duty: Warzone',
+                                     'Call of Duty: Modern Warfare 2 Campaign Remastered',
+                                     'Call of Duty: Black Ops Cold War',
+                                     'Call of Duty: Vanguard']}
 }
 
 
 def getinfo(request, name):
     if dct.get(name):
-        return HttpResponse(dct[name])
+        data = {
+            'name': name,
+            'intro': dct.get(name)['intro'],
+            'info': dct.get(name)['description']
+        }
+        return render(request, 'industry_names/my_industries.html', context=data)
     else:
         return HttpResponseRedirect('404')
 
 
 def getNotFound(request):
-    return HttpResponse(render_to_string('mygames.html'))
-
-
+    return HttpResponse(render_to_string('industry_names/mygames.html'))
